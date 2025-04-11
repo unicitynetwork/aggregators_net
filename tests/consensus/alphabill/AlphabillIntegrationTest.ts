76defcbb675886e174f16520035763d6a66324b2
Mart Mägi <mart.magi@guardtime.com>
2025-04-11 11:53:57 +0300

    Update exposed ports

--- tests/consensus/alphabill/AlphabillIntegrationTest.ts
+++ tests/consensus/alphabill/AlphabillIntegrationTest.ts
@@ -30,7 +30,7 @@ describe('Alphabill Client Integration Tests', () => {
   const privateKey = '1DE87F189C3C9E42F93C90C95E2AC761BE9D0EB2FD1CA0FF3A9CE165C3DE96A9';
   const alphabillSigningService = new DefaultSigningService(Base16Converter.decode(privateKey));
   const proofFactory = new PayToPublicKeyHashProofFactory(alphabillSigningService);
-  const tokenPartitionUrl = 'http://localhost:9001/rpc';
+  const tokenPartitionUrl = 'http://localhost:8003/rpc';
   const networkId = 3;
   const tokenPartitionId = 2;
 
@@ -43,7 +43,7 @@ describe('Alphabill Client Integration Tests', () => {
   let unicitySigningService: SigningService;
 
   beforeAll(async () => {
-    console.log('Setting up test environment with Alphabill root nodes, permissioned token partition and MongoDB...');
+    console.log('Setting up test environment with Alphabill root node, permissioned token partition node and MongoDB...');
     mongoContainer = await new MongoDBContainer('mongo:7').start();
     aggregatorEnvironment = await new DockerComposeEnvironment(composeFilePath, composeAlphabill)
       .withBuild()
